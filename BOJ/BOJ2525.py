43ca0b573663d1305370f3b49b44ec2c968edded
kjw <kjw55523@gmail.com>
2022-04-13 11:53:01 +0900

    BOJ2480

--- BOJ/BOJ2525.py
+++ BOJ/BOJ2525.py
@@ -24,5 +24,11 @@ else:
     print(a, d)
     
 
-
+# a,b = map(int,input().split())
+# c= int(input())
+# sum = a * 60 + b
+# sum = sum + c
+# if sum >= 1440:
+#     sum -= 1440
+# print('%d %d'(sum//60, sum % 60))
 
