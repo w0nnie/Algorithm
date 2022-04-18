e8ae972546259e62c3d91d169ac1a2740a2b501b
jinwon <kjw55523@gmail.com>
2022-04-18 23:12:20 +0900

    BOJ4344

---
+++ BOJ/BOJ4344.py
@@ -0,0 +1,17 @@
+# 대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 
+# 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+
+def solve():
+  c = int(input())
+  for _ in range(c):
+    avg = 0
+    count = 0
+    jumsu = list(map(int,input().split()))
+    avg = (sum(jumsu) - jumsu[0]) / jumsu[0]
+    for i in jumsu:
+      if i > avg:
+        count += 1
+        answer = count / jumsu[0] * 100
+    print("{:.3f}%".format(answer))
+
+solve()
